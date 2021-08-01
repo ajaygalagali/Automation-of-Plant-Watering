79dad906231fa6ebb7b4f33cfd062f58ae065d33
ajaygalagali <gvajay321@gmail.com>
2021-08-01 18:05:11 +0530

    added logs

--- main.ino
+++ main.ino
@@ -18,16 +18,14 @@ const int MOISTURE_SENSOR_THRESHOLD = 63; // If less, water plant
 // wd: WaterDepth Sensor Variables
 int DEPTH_SENSOR_PORT = A3; // Port Number
 int wdRawValue = 0;
-int wdPercentage = 0;
-
-const int DEPTH_SENSOR_MIN = 0;
-const int DEPTH_SENSOR_MAX = 0;
-const int DEPTH_SENSOR_THRESHOLD = 10;
+// Sensor value depends on type of water
+const int DEPTH_HIGH = 300;
+const int DEPTH_LOW = 260;
 
 // Water Pump
 const int WATER_PUMP_A_PORT = 2;
 const int WATER_PUMP_B_PORT = 3;
-const int WATER_FLOW_TIME = 1000; // In milli seconds
+const int WATER_FLOW_TIME = 500; // In milli seconds
 
 // Constants
 const int DELAY_ONE = 1000;
@@ -44,10 +42,13 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // Checking Water Source Level
-  if (isWaterSource) {
+  if (isWaterSource()) {
 
+      delay(DELAY_ONE);
     // Plant A
     if (readMoistureA() < MOISTURE_SENSOR_THRESHOLD) {
+
+      delay(DELAY_ONE);
       // Water Plant A
       waterPlantA();
 
@@ -60,19 +61,25 @@ void loop() {
 
     // Plant B
     if (readMoistureB() < MOISTURE_SENSOR_THRESHOLD) {
+      
+      delay(DELAY_ONE);
       // Water Plant B
       waterPlantB();
       // Notify user : Watered Plant B at <TimeStamp>
 
     } else {
       // Notify User: Plant B does not require water at <Timestamp>
+      Serial.println("Notify User: Plant B does not require water at <Timestamp>");
     }
 
 
   } else {
     // Notify User: Insufficient water source, please refill.
+    Serial.println("Notify User: Insufficient water source, please refill.");
 
   }
+  Serial.println("---------------------END of CYCLE---------------------");
+  delay(5000);
 
 
 }
@@ -81,6 +88,8 @@ int readMoistureA() {
   maRawValue = analogRead(MOISTURE_SENSOR_A_PORT);
   maPercentage = mapMoistureValue(maRawValue);
 
+  Serial.print("Reading Moisture A");
+  Serial.println(maPercentage);
   return maPercentage;
 }
 
@@ -88,26 +97,32 @@ int readMoistureB() {
   mbRawValue = analogRead(MOISTURE_SENSOR_B_PORT);
   mbPercentage = mapMoistureValue(mbRawValue);
 
+  Serial.print("Reading Moisture B: ");
+  Serial.println(mbPercentage); 
   return mbPercentage;
 }
 
 int readWaterDepth() {
   wdRawValue = analogRead(DEPTH_SENSOR_PORT);
-  wdPercentage = mapWaterDepthValue(wdRawValue);
 
-  return wdPercentage;
+  Serial.print("Reading Water Depth: ");
+  Serial.println(wdRawValue);
+  return wdRawValue;
 
 }
 
 bool isWaterSource() {
-  if (readWaterDepth() < DEPTH_SENSOR_THRESHOLD) {
+  
+  if (readWaterDepth() < DEPTH_LOW) {
+    Serial.println("isWaterSource : FALSE");
     return false;
   }
-
+  Serial.println("isWaterSource : TRUE");
   return true;
 }
 
 void waterPlantA() {
+  Serial.println("Watering Plant A");
   digitalWrite(WATER_PUMP_A_PORT, HIGH);
   delay(WATER_FLOW_TIME);
   digitalWrite(WATER_PUMP_A_PORT, LOW);
@@ -115,6 +130,7 @@ void waterPlantA() {
 }
 
 void waterPlantB() {
+  Serial.println("Watering Plant B");
   digitalWrite(WATER_PUMP_B_PORT, HIGH);
   delay(WATER_FLOW_TIME);
   digitalWrite(WATER_PUMP_B_PORT, LOW);
@@ -138,12 +154,6 @@ int mapMoistureValue(int rawValue) {
      90% = Wet Soil
   */
 
-  return map(rawValue, mMinValue, mMaxValue, 100, 0);
+  return map(rawValue, MOISTURE_SENSOR_MIN, MOISTURE_SENSOR_MAX, 100, 0);
 
-}
-
-
-// Maps water depth sensor value to 0 to 100 %
-int mapWaterDepthValue(int rawValue) {
-  return map(rawValue, wdMin, wdMax, 0, 100);
 }
